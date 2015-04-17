b515fc9302afae8ed6fb4f6c4341d7cf7dff9d1d
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-17 09:42:48 -0400

    loop working

--- TJ.py
+++ TJ.py
@@ -1,7 +1,16 @@
 import csv
+from votelist import house_votes, senate_votes
+
+# with open('congress_votes_1-1_h35.csv', 'rb') as csvfile:
+#     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#     for row in spamreader:
+#         print " ".join(row)
 
 
-with open ("congress_votes_1-1_h32.csv", 'rb') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print " ".join(row)
+
+
+for vote in house_votes:
+	with open(vote, 'rb') as csvfile:
+	    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+	    for row in spamreader:
+	        print " ".join(row)
