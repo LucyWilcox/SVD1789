fbd97d37c3a441b75e2df816f387da55cf1857a4
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-17 10:52:35 -0400

    pre pull

--- TJ.py
+++ TJ.py
@@ -1,11 +1,19 @@
 import csv
 from votelist import house_votes, senate_votes
+from numpy import matrix
+
+
 
 # with open('congress_votes_1-1_h35.csv', 'rb') as csvfile:
 #     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 #     for row in spamreader:
 #         print " ".join(row)
 
+class Voter(object):
+	__init__(self, name, location, voting_record):
+		self.name = name
+		self.location = location
+		self.voting_record = voting_record
 
 
 
