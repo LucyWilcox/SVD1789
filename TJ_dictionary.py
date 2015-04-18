94ccfbd319da6a02c9815fb80d94a0d6d38a4425
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2015-04-18 13:15:35 -0400

    dictionary new edition - working on it

--- TJ_dictionary.py
+++ TJ_dictionary.py
@@ -1,6 +1,7 @@
 import csv
 from votelist import house_votes, senate_votes
 
+
 def check_dictionary(votes, name):
 	if name in votes:
 		return True
@@ -10,10 +11,11 @@ def check_dictionary(votes, name):
 def get_votes_names(data):
 	name = {}
 	for entry in data:
-		votes = {}
+
 		data_list = data[entry]
 		bill = entry
 		for vote in data_list:
+			votes = {}
 			vote = vote.replace(","," ").split()
 			if vote[3] == 'Yea':
 				
@@ -44,6 +46,53 @@ def get_votes_names(data):
 
 	return votes
 
+def create_voter_list(house_dict):
+	"""Generates a list of voters names """
+	voters = []
+	for key in sorted(house_dict):
+		voters.append(key)
+	return voters
+
+def  name_dictionary(list_of_names):
+	print list_of_names
+	names  = {}
+	votes = {}
+	for name in list_of_names:
+		if name in names:
+			pass
+		else:
+			names[name] =  votes
+
+	return names
+
+def vote_dictionary(data,names):
+
+	for entry in data:
+
+		data_list = data[entry]
+		bill = entry
+		for vote in data_list:
+			votes = {}
+			vote = vote.replace(","," ").split()
+			if vote[3] == 'Yea':
+				person = str(vote[4]+' '+vote[5]) 
+				votes[bill] = True
+				name[person]  = votes
+			
+			elif vote[3] == 'Nay':
+				
+				person = vote[4]+' '+vote[5]
+				votes[bill] = True
+				name[person]  = votes
+
+			elif vote[3] == 'Not':
+				
+				person = vote[5]+' '+vote[6]
+				votes[bill] = True
+				name[person]  = votes
+
+	return names	
+
 def get_data(house_votes):
 	data = {}
 
@@ -61,5 +110,9 @@ def get_data(house_votes):
 
 if __name__ == '__main__':
 	data = get_data(house_votes)
-	print get_votes_names(data)
+	house_dict = get_votes_names(data)
+	print house_dict
+	list_of_names = create_voter_list(house_dict)
+	names = name_dictionary(list_of_names)
+	print vote_dictionary(data,names)
 
